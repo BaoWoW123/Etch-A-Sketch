b89a6d4bfecc37a209d53312118700c9e5dabcb2
BaoWoW <baonguyen9392@gmail.com>
2022-08-26 14:53:34 -0700

    Created Html, Js, & Css Files

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector('#grid')
+const gridbox = document.createElement('div');
+gridbox.classList.add('gridbox');
+
+
+gridbox.textContent = 'oholafs'
+grid.appendChild('gridbox');
