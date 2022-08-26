7f86db7d9291472c31c1057909e0066df019f48b
BaoWoW <baonguyen9392@gmail.com>
2022-08-26 15:41:41 -0700

    Made Css Grid With Functional Etching
    Created 256 divs with loop, but is not in a 16x16 grid.
    Hovering over divs changes background color to black.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
-const grid = document.querySelector('#grid')
-const gridbox = document.createElement('div');
-gridbox.classList.add('gridbox');
 
 
-gridbox.textContent = 'oholafs'
-grid.appendChild('gridbox');
+for (i = 0; i < 256; i++) {
+    const grid = document.querySelector('#grid')
+    const gridbox = document.createElement('div');
+    gridbox.classList.add('gridbox');
+
+ 
+    grid.appendChild(gridbox);
+
+    gridbox.addEventListener('mouseover', () => {
+        gridbox.style.backgroundColor = 'black';
+    })
+}
