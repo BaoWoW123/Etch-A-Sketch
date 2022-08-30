e7ea8edae37ec0194fa4518b986537a6cf22ca5b
BaoWoW <baonguyen9392@gmail.com>
2022-08-30 13:56:55 -0700

    Added Title & Button
    Made title and button to turn etching on/off.
    Button doesn't work as intended.
    When button is off, etching still occurs.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,31 @@
 
+const title = document.querySelector('.title');
+const button = document.createElement('button');
+button.classList.add('button');
+button.textContent = 'Etch: Off';
+title.appendChild(button);
+
+button.addEventListener('click', (etch) => {
+    if (button.textContent === 'Etch: Off') {
+        return button.textContent = 'Etch: On';
+    }
+    else {
+        return button.textContent = 'Etch: Off';
+    }
+})
+
 
 for (i = 0; i < 256; i++) {
     const grid = document.querySelector('#grid')
     const gridbox = document.createElement('div');
     gridbox.classList.add('gridbox');
-
- 
     grid.appendChild(gridbox);
 
-    gridbox.addEventListener('mouseover', () => {
-        gridbox.style.backgroundColor = 'black';
-    })
+    button.addEventListener('click', () => {
+    if (button.textContent === 'Etch: On') {
+            gridbox.addEventListener('mouseover', () => {
+            gridbox.style.backgroundColor = 'black';
+        })
+        }
+    })   
 }
