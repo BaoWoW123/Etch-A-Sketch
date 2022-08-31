675ad5f3309549d057657169b13e35a7a91e1bd8
BaoWoW <baonguyen9392@gmail.com>
2022-08-31 16:00:05 -0700

    Added Etching Modes On, Off, & Erase

--- javascript.js
+++ javascript.js
@@ -9,23 +9,32 @@ button.addEventListener('click', (etch) => {
     if (button.textContent === 'Etch: Off') {
         return button.textContent = 'Etch: On';
     }
+    else if (button.textContent === 'Etch: On') {
+        return button.textContent = 'Erase';
+    }
     else {
         return button.textContent = 'Etch: Off';
     }
-})
 
+})
 
 for (i = 0; i < 256; i++) {
     const grid = document.querySelector('#grid')
     const gridbox = document.createElement('div');
     gridbox.classList.add('gridbox');
     grid.appendChild(gridbox);
-
+    let eventHandler = (() => gridbox.style.backgroundColor = 'black')
     button.addEventListener('click', () => {
-    if (button.textContent === 'Etch: On') {
-            gridbox.addEventListener('mouseover', () => {
-            gridbox.style.backgroundColor = 'black';
-        })
+    if (button.textContent == 'Etch: On') {
+            gridbox.addEventListener('mouseover', (eventHandler))
         }
+    if (button.textContent == 'Etch: Off') {
+            gridbox.removeEventListener('mouseover', (eventHandler))
+        }
+    if (button.textContent == 'Erase') {
+        gridbox.addEventListener('mouseover', (erase) => {
+            gridbox.style.backgroundColor ='white';
+        })
+    }
     })   
 }
